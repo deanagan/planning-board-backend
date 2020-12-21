1f0873fbc63a2fd9b7c0609f8ef5b388c276eaef
Dean Agan <agandfr@gmail.com>
2020-12-21 13:29:30 +1100

    Changes to make head changeable

---
+++ src/home/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div>
+      <img
+        class="character"
+        src="@/assets/yoda.png"
+        alt="yoda"
+        aria-hidden="true"
+      />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your greeting card!
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HomePage extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.character {
+  height: 300px;
+}
+</style>
