c8140fe0da426c5b644478317f7c46e042a52e5d
Dean Agan <agandfr@gmail.com>
2021-01-20 20:55:28 +1100

    Add next part and previous part methods

--- src/core/PartSelector.vue
+++ src/core/PartSelector.vue
@@ -10,10 +10,30 @@ import { PropType } from "vue";
 @Component
 export default class PartSelector extends Vue {
   @Prop({
-    type: Object as PropType<Part>,
-    required: true
+    type: Object as PropType<Array<Part>>,
+    required: true,
+    default: () => []
   })
-  part!: Part;
+  parts!: Array<Part>;
+  selectedIndex = 0;
+
+  getNextPart(): number {
+    if (this.parts.length - 1 > this.selectedIndex) {
+      this.selectedIndex += 1;
+    } else {
+      this.selectedIndex = 0;
+    }
+    return this.selectedIndex;
+  }
+
+  getPreviousPart(): number {
+    if (this.selectedIndex > 0) {
+      this.selectedIndex -= 1;
+    } else {
+      this.selectedIndex = this.parts.length - 1;
+    }
+    return this.selectedIndex;
+  }
 }
 </script>
 
