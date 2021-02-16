351eb0c3a8da853ec1639e990b19e95994dfa03b
Dean Agan <agandfr@gmail.com>
2021-02-16 20:22:56 +1100

    Create a part info component to show when clicking a part

---
+++ src/core/PartInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ part.title }}</h1>
+    <div>
+      {{ part.desc }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import parts from "@/data/data";
+
+interface PartInfo {
+  title: string;
+  desc: string;
+}
+export default Vue.extend({
+  name: "PartInfo",
+  computed: {
+    part(): PartInfo {
+      const { partType, id } = this.$route.params;
+      const part = parts[partType].find(part => part.id === +id);
+
+      return {
+        title: part?.title ?? "No title found",
+        desc: part?.description ?? "No description found"
+      };
+    }
+  }
+});
+</script>
