9513519237ff41975c5e1e3f340a0240c34df029
Dean Agan <agandfr@gmail.com>
2021-02-18 19:37:40 +1100

    Add browse parts to the router and set the other components as its children

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,11 @@ import Router from "vue-router";
 import HomePage from "@/home/HomePage.vue";
 import CardBuilder from "@/core/CardBuilder.vue";
 import PartInfo from "@/core/PartInfo.vue";
+import BrowseParts from "@/core/BrowseParts.vue";
+import CardArms from "@/core/CardArms.vue";
+import CardHeads from "@/core/CardHeads.vue";
+import CardBases from "@/core/CardBases.vue";
+import CardTorsos from "@/core/CardTorsos.vue";
 
 Vue.use(Router);
 
@@ -17,6 +22,34 @@ export default new Router({
       path: "/build",
       name: "Build",
       component: CardBuilder
+    },
+    // This must come before part info below
+    {
+      path: "/parts/browse",
+      name: "BrowseParts",
+      component: BrowseParts,
+      children: [
+        {
+          name: "BrowseHeads",
+          path: "heads",
+          component: CardHeads
+        },
+        {
+          name: "BrowseArms",
+          path: "arms",
+          component: CardArms
+        },
+        {
+          name: "BrowseBases",
+          path: "bases",
+          component: CardBases
+        },
+        {
+          name: "BrowseTorsos",
+          path: "torsos",
+          component: CardTorsos
+        }
+      ]
     },
     {
       path: "/parts/:partType/:id",
