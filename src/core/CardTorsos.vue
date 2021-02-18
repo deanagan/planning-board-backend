d02a66a29fd7a50419753ed58757c28d9b0d2820
Dean Agan <agandfr@gmail.com>
2021-02-18 19:37:13 +1100

    Add some components for a browse page

---
+++ src/core/CardTorsos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Torsos</h2>
+    Describe my torsos
+    <div v-for="(part, idx) in parts" :key="idx">
+      <h4>{{ part.title }}</h4>
+      <div>{{ part.description }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import parts from "@/data/data";
+
+export default Vue.extend({
+  name: "CardTorsos",
+  data: () => {
+    return { parts: parts.torsos };
+  }
+});
+</script>
