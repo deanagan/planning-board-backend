ae4777a66885751c048e395fe51abed65416dfc2
Dean Agan <agandfr@gmail.com>
2021-02-24 20:40:11 +1100

    Create index.ts store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,16 @@
+import Built from "@/common/built";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-
-export default new Vuex.Store({});
+// TODO: Typescript approach
+export default new Vuex.Store({
+  state: {
+    gallery: [] as Built[]
+  },
+  mutations: {
+    addDroidToGallery(state, droid) {
+      state.gallery.push(droid);
+    }
+  }
+});
