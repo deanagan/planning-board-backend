0751adf1d666955bfefc45f92cba56f53a4c84cc
Dean Agan <agandfr@gmail.com>
2021-03-17 19:31:19 +1100

    Add dropzone to declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,4 @@
 
 declare module 'reveal.js/dist/reveal';
-
-declare module 'vue-image-upload-resize';
+declare module 'vue2-dropzone/dist/vue2Dropzone';
+declare module 'vue-image-upload-resize'; // TODO: Remove me
