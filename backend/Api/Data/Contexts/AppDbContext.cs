f49f1af86b3f58470401c23e330f2b5128ec8996
Dean Agan <agandfr@gmail.com>
2021-04-06 09:15:24 +1000

    Add relationships and seeding

---
+++ backend/Api/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,71 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Api.Data.Models;
+
+namespace Api.Data.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TaskDetail> TaskDetails { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasKey(user => user.Id);
+
+            modelBuilder.Entity<User>()
+                 .HasOne(a => a.Role);
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role
+                {
+                    Id = 1,
+                    Name = "Scrum Master",
+                    Description = "A scrum master has more administration rights."
+                },
+                new Role
+                {
+                    Id = 2,
+                    Name = "Developer",
+                    Description = "A developer can do basic operations."
+                }
+            );
+
+            modelBuilder.Entity<User>().HasData(
+                new User {
+                    Id = 1,
+                    Name = "Jack Black",
+                    RoleId = 1,
+                    Email = "jack.black@planningboard.io",
+                    Hash = "123456"
+                },
+                new User {
+                    Id = 2,
+                    Name = "John Smith",
+                    RoleId = 2,
+                    Email = "john.smith@planningboard.io",
+                    Hash = "12345678"
+                },
+                new User {
+                    Id = 3,
+                    Name = "Jane Doe",
+                    RoleId = 2,
+                    Email = "jane.doe@planningboard.io",
+                    Hash = "988767"
+                }
+            );
+        }
+
+    }
+}
