c2cc37b13be61554bde5cb17293834a7b572ae71
Dean Agan <agandfr@gmail.com>
2021-04-07 11:18:05 +1000

    Implement new userview into service

--- backend/Api/Services/UserService.cs
+++ backend/Api/Services/UserService.cs
@@ -13,18 +13,33 @@ namespace Api.Services
             _unitOfWork = unitOfWork;
         }
 
-        public IEnumerable<User> GetUsers()
+        public IEnumerable<UserView> GetUsers()
         {
-            return _unitOfWork.Users.GetAll();
+            var users = from user in _unitOfWork.Users.GetAll()
+            join role in _unitOfWork.Roles.GetAll()
+            on user.RoleId equals role.Id
+            select new UserView {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Hash = user.Hash,
+                Role = role
+            };
+
+            return users;
         }
 
-        public User GetUser(int id)
+        public UserView GetUser(int id)
         {
             var user = _unitOfWork.Users.Get(id);
 
-            user.Role = _unitOfWork.Roles.Get(user.RoleId);
-
-            return user;
+            return new UserView {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Hash = user.Hash,
+                Role = _unitOfWork.Roles.Get(user.RoleId)
+            };
         }
 
 
