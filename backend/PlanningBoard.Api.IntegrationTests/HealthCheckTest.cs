a5b1f249f9ceb8dbb381eae00819d45d52b594ad
Dean Agan <agandfr@gmail.com>
2021-04-19 19:59:19 +1000

    Adding health check test

--- backend/PlanningBoard.Api.IntegrationTests/HealthCheckTest.cs
+++ backend/PlanningBoard.Api.IntegrationTests/HealthCheckTest.cs
@@ -1,6 +1,29 @@
-using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace Planningboard.Api.IntegrationTests
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using Xunit;
+
+using FluentAssertions;
+
+namespace PlanningBoard.Api.IntegrationTests
 {
+    public class HealthCheckTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+        public HealthCheckTest(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
 
+        [Fact]
+        public async Task HealthCheck_ReturnsOk()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
 }
